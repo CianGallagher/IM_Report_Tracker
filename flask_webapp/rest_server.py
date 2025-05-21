4296db64e34ae95823159405877cfbc5d1ff9d0a
Cian Gallagher <ciangall1@gmail.com>
2025-05-21 20:10:01 +0100

    database class defined using sqlalchemy syntax

--- flask_webapp/rest_server.py
+++ flask_webapp/rest_server.py
@@ -1,18 +1,27 @@
+from datetime import date
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
-# SQLite database defined
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///reports.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# Initializes the database
 db = SQLAlchemy(app)
+
+class Report(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    Investment_advisor_name = db.Column(db.String(100), nullable=False)
+    fund_name = db.Column(db.String(100), nullable=False)
+    sub_fund_name = db.Column(db.String(100), nullable=False)
+    delegate_name = db.Column(db.String(100), nullable=False)
+    reporting_period = db.Column(db.String(20), nullable=False)
+    report_date = db.Column(db.Date, nullable=False, default=date.today)
+    status = db.Column(db.String(20), nullable=False)
+    comments = db.Column(db.String(200), nullable=True)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
